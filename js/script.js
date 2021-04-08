0295603e10d9cb6afc19c06b717477d4b099c3d4
Lorena Spiller <lorenaspiller@gmail.com>
2021-04-08 16:02:59 +0200

    stampati a schermo album tramite API

--- js/script.js
+++ js/script.js
@@ -13,6 +13,12 @@ BONUS 2: Ordinare i dischi per anno di uscita.
 var app = new Vue({
   el: '#app',
   data: {
-
+    albums: []
+  },
+  mounted: function() {
+    axios.get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((response)=>{
+        this.albums = response.data.response;
+      });
   }
 })
